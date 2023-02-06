79c36a525f8c2c9d29f1c5540938922dbd93a9c3
Maple <teacha8611@gmail.com>
2023-02-06 22:12:47 +0800

    Update 1470.js

--- JS/1470.js
+++ JS/1470.js
@@ -4,19 +4,15 @@
  * @return {number[]}
  */
 var shuffle = function(nums, n) {
-    let tmp, arr = new Array(2 * n);
+    let arr = [];
 
-    for(let i = 0; i < 2 * n; i++) {
-        // y
-        if(i % 2 == 1) {
-            tmp = parseInt(i / 2 + n);
-            arr[i] = nums[tmp];
-        }
-        // x
-        else {
-            arr[i] = nums[i / 2];
-        }
+    for(let i = 0; i < n; i++) {
+        arr.push(nums[i], nums[i+n]);
     }
 
     return arr;
 };
+
+/* 
+想法：把陣列的東西放進去！
+複雜度：O(n)
