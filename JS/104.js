52c89113b231d8423b45009a41171a619716aba1
Maple <teacha8611@gmail.com>
2023-02-16 20:29:30 +0800

    Create 104.js

---
+++ JS/104.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if(root === null) return 0;
+
+    let left = maxDepth(root.left);
+    let right = maxDepth(root.right);
+
+    return Math.max(left, right) + 1;
+}; 
+/*
+
+*/
+題意:給二元樹找樹深。
+想法:遞迴找。
+複雜度:O(n)。n為節點，因為有可能是鏈。
+*/
